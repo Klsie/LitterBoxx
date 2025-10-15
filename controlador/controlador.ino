f67790a64289f7ec5224d9eefc49cdefb7419f51
Teisel <lesiet02@gmail.com>
2025-10-15 08:23:15 -0600

    hardwareCompleto

--- controlador/controlador.ino
+++ controlador/controlador.ino
@@ -1,12 +1,39 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <HX711_ADC.h>
+#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
+#include <EEPROM.h>
+#endif
 
 // Credenciales WiFi
-const char* ssid = "Fell_dragon_grima";
-const char* password = "Peni_parker_b3st";
+//const char* ssid = "Fell_dragon_grima";
+//const char* password = "Peni_parker_b3st";
+
+const char* ssid = "saquenmedeaqui";
+const char* password = "12345678";
+
+//Sensor de peso
+const int HX711_dout = 4; //mcu > HX711 dout pin
+const int HX711_sck = 5; //mcu > HX711 sck pin
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+const int calVal_eepromAdress = 0;
+unsigned long t = 0;
+
+//Sensor ultrasonido
+const int TRIG = 18;
+const int ECO = 19;
+int duracion;
+int distancia;
+
+//Motor a pasos
+const int paso1 = 13;
+const int paso2 = 12;
+const int paso3 = 14;
+const int paso4 = 27;
+
 
 // Dirección de tu API en Azure
-String serverName = "https://areneroiot-dhdbb9hcc0a0g3cs.canadacentral-01.azurewebsites.net/api/datos";
+String serverName = "https://areneroiot-dhdbb9hcc0a0g3cs.canadacentral-01.azurewebsites.net";
 
 void setup() {
   Serial.begin(115200);
@@ -20,9 +47,71 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("\nConectado a WiFi");
+
+  //Iniciar balanza
+  LoadCell.begin();
+  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag() || LoadCell.getSignalTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+  else {
+    LoadCell.setCalFactor(1.0); // user set calibration value (float), initial value 1.0 may be used for this sketch
+    Serial.println("Startup is complete");
+  }
+  while (!LoadCell.update());
+  calibrate();
+
+  //Iniciar sensor ultrasonido
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECO, INPUT);
+
+  //Iniciar motor a pasos
+  pinMode(paso1, OUTPUT);
+  pinMode(paso2, OUTPUT);
+  pinMode(paso3, OUTPUT);
+  pinMode(paso4, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(TRIG, HIGH); 		
+  delay(1);				
+  digitalWrite(TRIG, LOW);		
+  
+  duracion = pulseIn(ECO, HIGH);	
+  distancia = duracion / 58.2;		
+  Serial.println(distancia);
+
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 0;
+
+  if (LoadCell.update()) newDataReady = true;
+  if (newDataReady) {
+    if (millis() > t + serialPrintInterval) {
+      float i = LoadCell.getData();
+      Serial.print("Load_cell output val: ");
+      i = i / 1000;
+      Serial.println(i);
+      newDataReady = 0;
+      t = millis();
+    }
+  }
+
+  // receive command from serial terminal
+  if (Serial.available() > 0) {
+    char inByte = Serial.read();
+    if (inByte == 't') LoadCell.tareNoDelay(); //tare
+    else if (inByte == 'r') calibrate(); //calibrate
+    else if (inByte == 'c') changeSavedCalFactor(); //edit calibration value manually
+  }
+
+  // check if last tare operation is complete
+  if (LoadCell.getTareStatus() == true) {
+    Serial.println("Tare complete");
+  }
+
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
 
@@ -50,5 +139,206 @@ void loop() {
     Serial.println("WiFi desconectado");
   }
 
-  delay(10000); // Espera 10 seg antes de mandar otra petición
+  //vuelta(10);
+
+
+  delay(1000); // Espera 10 seg antes de mandar otra petición
+}
+
+void vuelta(int veces)
+{
+  digitalWrite(paso1, HIGH);
+  digitalWrite(paso2, LOW);
+  digitalWrite(paso3, LOW);
+  digitalWrite(paso4, LOW);
+
+  delay(500);
+
+  digitalWrite(paso1, HIGH);
+  digitalWrite(paso2, HIGH);
+  digitalWrite(paso3, LOW);
+  digitalWrite(paso4, LOW);
+
+  delay(500);
+
+  digitalWrite(paso1, LOW);
+  digitalWrite(paso2, HIGH);
+  digitalWrite(paso3, LOW);
+  digitalWrite(paso4, LOW);
+
+  delay(500);
+
+  digitalWrite(paso1, LOW);
+  digitalWrite(paso2, HIGH);
+  digitalWrite(paso3, HIGH);
+  digitalWrite(paso4, LOW);
+
+  delay(500);
+
+  digitalWrite(paso1, LOW);
+  digitalWrite(paso2, LOW);
+  digitalWrite(paso3, HIGH);
+  digitalWrite(paso4, LOW);
+
+  delay(500);
+
+  digitalWrite(paso1, LOW);
+  digitalWrite(paso2, LOW);
+  digitalWrite(paso3, HIGH);
+  digitalWrite(paso4, HIGH);
+
+  delay(500);
+
+  digitalWrite(paso1, LOW);
+  digitalWrite(paso2, LOW);
+  digitalWrite(paso3, LOW);
+  digitalWrite(paso4, HIGH);
+
+  delay(500);
+
+  digitalWrite(paso1, HIGH);
+  digitalWrite(paso2, LOW);
+  digitalWrite(paso3, LOW);
+  digitalWrite(paso4, HIGH);
+
+  delay(500);
+
+  if (veces > 0)
+  {
+    vuelta(veces - 1);
+  }
+}
+
+void calibrate() {
+  Serial.println("***");
+  Serial.println("Start calibration:");
+  Serial.println("Place the load cell an a level stable surface.");
+  Serial.println("Remove any load applied to the load cell.");
+  Serial.println("Send 't' from serial monitor to set the tare offset.");
+
+  boolean _resume = false;
+  while (_resume == false) {
+    LoadCell.update();
+    if (Serial.available() > 0) {
+      if (Serial.available() > 0) {
+        char inByte = Serial.read();
+        if (inByte == 't') LoadCell.tareNoDelay();
+      }
+    }
+    if (LoadCell.getTareStatus() == true) {
+      Serial.println("Tare complete");
+      _resume = true;
+    }
+  }
+
+  Serial.println("Now, place your known mass on the loadcell.");
+  Serial.println("Then send the weight of this mass (i.e. 100.0) from serial monitor.");
+
+  float known_mass = 0;
+  _resume = false;
+  while (_resume == false) {
+    LoadCell.update();
+    if (Serial.available() > 0) {
+      known_mass = Serial.parseFloat();
+      if (known_mass != 0) {
+        Serial.print("Known mass is: ");
+        Serial.println(known_mass);
+        _resume = true;
+      }
+    }
+  }
+
+  LoadCell.refreshDataSet(); //refresh the dataset to be sure that the known mass is measured correct
+  float newCalibrationValue = LoadCell.getNewCalibration(known_mass); //get the new calibration value
+
+  Serial.print("New calibration value has been set to: ");
+  Serial.print(newCalibrationValue);
+  Serial.println(", use this as calibration value (calFactor) in your project sketch.");
+  Serial.print("Save this value to EEPROM adress ");
+  Serial.print(calVal_eepromAdress);
+  Serial.println("? y/n");
+
+  _resume = false;
+  while (_resume == false) {
+    if (Serial.available() > 0) {
+      char inByte = Serial.read();
+      if (inByte == 'y') {
+#if defined(ESP8266)|| defined(ESP32)
+        EEPROM.begin(512);
+#endif
+        EEPROM.put(calVal_eepromAdress, newCalibrationValue);
+#if defined(ESP8266)|| defined(ESP32)
+        EEPROM.commit();
+#endif
+        EEPROM.get(calVal_eepromAdress, newCalibrationValue);
+        Serial.print("Value ");
+        Serial.print(newCalibrationValue);
+        Serial.print(" saved to EEPROM address: ");
+        Serial.println(calVal_eepromAdress);
+        _resume = true;
+
+      }
+      else if (inByte == 'n') {
+        Serial.println("Value not saved to EEPROM");
+        _resume = true;
+      }
+    }
+  }
+
+  Serial.println("End calibration");
+  Serial.println("***");
+  Serial.println("To re-calibrate, send 'r' from serial monitor.");
+  Serial.println("For manual edit of the calibration value, send 'c' from serial monitor.");
+  Serial.println("***");
+}
+
+void changeSavedCalFactor() {
+  float oldCalibrationValue = LoadCell.getCalFactor();
+  boolean _resume = false;
+  Serial.println("***");
+  Serial.print("Current value is: ");
+  Serial.println(oldCalibrationValue);
+  Serial.println("Now, send the new value from serial monitor, i.e. 696.0");
+  float newCalibrationValue;
+  while (_resume == false) {
+    if (Serial.available() > 0) {
+      newCalibrationValue = Serial.parseFloat();
+      if (newCalibrationValue != 0) {
+        Serial.print("New calibration value is: ");
+        Serial.println(newCalibrationValue);
+        LoadCell.setCalFactor(newCalibrationValue);
+        _resume = true;
+      }
+    }
+  }
+  _resume = false;
+  Serial.print("Save this value to EEPROM adress ");
+  Serial.print(calVal_eepromAdress);
+  Serial.println("? y/n");
+  while (_resume == false) {
+    if (Serial.available() > 0) {
+      char inByte = Serial.read();
+      if (inByte == 'y') {
+#if defined(ESP8266)|| defined(ESP32)
+        EEPROM.begin(512);
+#endif
+        EEPROM.put(calVal_eepromAdress, newCalibrationValue);
+#if defined(ESP8266)|| defined(ESP32)
+        EEPROM.commit();
+#endif
+        EEPROM.get(calVal_eepromAdress, newCalibrationValue);
+        Serial.print("Value ");
+        Serial.print(newCalibrationValue);
+        Serial.print(" saved to EEPROM address: ");
+        Serial.println(calVal_eepromAdress);
+        _resume = true;
+      }
+      else if (inByte == 'n') {
+        Serial.println("Value not saved to EEPROM");
+        _resume = true;
+      }
+    }
+  }
+  Serial.println("End change calibration value");
+  Serial.println("***");
 }
