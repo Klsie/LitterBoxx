7989093e380e8851325f97a1ffb99c3290b16c65
Teisel <lesiet02@gmail.com>
2025-11-12 02:09:47 -0600

    DiseñoAlfa

--- Arenero2/app/src/main/java/com/example/arenero/screens/Home.kt
+++ Arenero2/app/src/main/java/com/example/arenero/screens/Home.kt
@@ -1,5 +1,6 @@
 package com.example.arenero.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -21,20 +22,31 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.arenero.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeArena()
 {
+    val context = LocalContext.current
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -78,6 +90,65 @@ fun HomeArena()
                 modifier = Modifier.align(Alignment.CenterHorizontally)
             )
 
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.grafica),
+                contentDescription = "Grafica",
+                modifier = Modifier
+                    .size(300.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            SectionButton("Limpiar", Color(0xFF7A858F), R.drawable.pala) {
+                Toast.makeText(context,"Funcionalidad por implementar",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
+
+@Composable
+fun SectionButton(
+    text: String,
+    color: Color,
+    image: Int,
+    onClick: () -> Unit
+) {
+    var isClickable by remember { mutableStateOf(true) }
+    val coroutineScope = rememberCoroutineScope()
+
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 24.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp))
+            .background(color)
+            .clickable(enabled = isClickable) {
+                isClickable = false
+                onClick()
+                coroutineScope.launch {
+                    delay(1000)
+                    isClickable = true
+                }
+            }
+            .padding(20.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp)
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                text = text,
+                fontSize = 22.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
